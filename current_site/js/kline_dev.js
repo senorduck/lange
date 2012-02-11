6dd0add319d79b7ea9f9398ba97f7c4eb00408f8
Ben Haley <senorduck@gmail.com>
2012-02-11 11:38:39 -0600

    Add current HTML-only site for February refresh

---
+++ current_site/js/kline_dev.js
@@ -0,0 +1,19 @@
+function narf(){
+	$("#contact")
+		.mouseenter(function(){
+			$(this).find("ul").stop(true,false).animate({height:"74"},300);
+		})
+		.mouseleave(function(){
+			$(this).find("ul").stop(true,false).animate({height:"0"},300);
+		});
+}
+function work(){
+	$(".project")
+		.mouseenter(function(){
+			$(this).find("img.on").fadeIn(600);
+		})
+		.mouseleave(function(){
+			$(this).find("img.on").fadeOut(600);
+		});
+	$(".project a").lightBox({imageBtnClose:'img/btn_close.gif',imageBtnPrev:'img/btn_prev.gif',imageBtnNext:'img/btn_next.gif'});
+}
