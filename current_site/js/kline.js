af1b77279995e5718b23e49ed28b0f6127075032
Ben Haley <senorduck@gmail.com>
2012-02-19 14:54:07 -0600

    Minor modifications to JS/CSS transitions

--- current_site/js/kline.js
+++ current_site/js/kline.js
@@ -1,32 +1,30 @@
 (function() {
 
   $(function() {
-    var showProjectLinks;
-    this.elemsToTransition = $(".no-csstransitions");
-    this.contactMenu = this.elemsToTransition.find("#contact");
-    this.projects = this.elemsToTransition.find(".project");
-    this.projectLinks = $(".project").find("a");
-    showProjectLinks = this.projectLinks.length > 0;
-    this.contactMenu.bind("mouseenter", function(event) {
-      return $(this).find("ul").animate({
-        height: '74px'
-      }, 300);
-    }).bind("mouseleave", function(event) {
-      return $(this).find("ul").animate({
-        height: 0
-      }, 300);
-    });
-    this.projects.bind("mouseenter", function(event) {
-      return $(this).find("img.on").animate({
-        opacity: 1.0
-      }, 600);
-    }).bind("mouseleave", function(event) {
-      return $(this).find("img.on").animate({
-        opacity: 0
-      }, 600);
-    });
+    var contactMenu, elemsToTransition, projectLinks, projects, showProjectLinks;
+    projectLinks = $(".project").find("a");
+    showProjectLinks = projectLinks.length > 0;
+    elemsToTransition = $(".no-csstransitions").length > 0;
+    if (elemsToTransition) {
+      contactMenu = $("#contact");
+      projects = $(".project");
+      contactMenu.bind("mouseenter", function(event) {
+        return $(this).find("ul").animate({
+          height: '74px'
+        }, 300);
+      }).bind("mouseleave", function(event) {
+        return $(this).find("ul").animate({
+          height: 0
+        }, 300);
+      });
+      projects.bind("mouseenter", function(event) {
+        return $(this).find("img.on").fadeIn(600);
+      }).bind("mouseleave", function(event) {
+        return $(this).find("img.on").fadeOut(600);
+      });
+    }
     if (showProjectLinks) {
-      return this.projectLinks.lightBox({
+      return projectLinks.lightBox({
         imageBtnClose: 'img/btn_close.gif',
         imageBtnPrev: 'img/btn_prev.gif',
         imageBtnNext: 'img/btn_next.gif'
